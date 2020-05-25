f8d54eb0cf61e33de580a230aad957d8d939a697
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2020-05-25 21:57:58 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -6,5 +6,5 @@ export default {
   PORT: process.env.PORT || 5000,
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
-  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'AXT3CbwdIBtAc-iL6biDA8vWZUR_ZMpW01EKPtCJZiPoGedfugjz69L_PfLaf13nMbTFYNzP_WUb3raZ',
 };
